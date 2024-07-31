51dc8992208216a47885b43f7a751d1f9acb6036
MatthewSawatz <msawatzky2@rrc.ca>
2024-07-31 11:48:25 -0500

    user registration page username form element working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   # before_action :set_card_sets
-  before_action :authenticate_user!, except: [:index, :show]
-    before_action :load_cart
+  before_action :authenticate_user!
+  before_action :load_cart
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = [:username, :email]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
   # private
 
   # def set_card_sets
